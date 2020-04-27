edde1a20a4ff0829f2a78a93f614dd27fb148c53
Birger Topphol <btopphol@hotmail.com>
2020-04-27 22:00:35 +0200

    Add cover to album view

--- app/src/main/java/no/birg/albumselector/AlbumFragment.kt
+++ app/src/main/java/no/birg/albumselector/AlbumFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.fragment_album.view.*
 import kotlinx.coroutines.*
 
@@ -34,6 +35,13 @@ class AlbumFragment(album: Album) : Fragment() {
             withContext(Dispatchers.Main) {
                 view.artist_name.text =
                     albumDetails.getJSONArray("artists").getJSONObject(0).getString("name")
+
+                val imageUrl = albumDetails.getJSONArray("images").getJSONObject(0).getString("url")
+                if (!imageUrl.isNullOrEmpty()) {
+                    Glide.with(view.context)
+                        .load(imageUrl)
+                        .into(view.album_cover)
+                }
             }
         }
         return view
