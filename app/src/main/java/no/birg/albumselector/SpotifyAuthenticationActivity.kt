5bb3eba12d27f15ded740aebfbad522fb21f6fb2
Birger Topphol <btopphol@hotmail.com>
2020-07-31 00:57:10 +0200

    Change SpotifyConnection to Service

---
+++ app/src/main/java/no/birg/albumselector/SpotifyAuthenticationActivity.kt
@@ -0,0 +1,36 @@
+package no.birg.albumselector
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import com.spotify.sdk.android.authentication.AuthenticationClient
+import com.spotify.sdk.android.authentication.AuthenticationRequest
+import com.spotify.sdk.android.authentication.AuthenticationResponse
+
+class SpotifyAuthenticationActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val request = AuthenticationRequest.Builder(Constants.CLIENT_ID, AuthenticationResponse.Type.TOKEN, Constants.REDIRECT_URI)
+            .setShowDialog(false)
+            .setScopes(arrayOf("user-read-email", "user-read-private", "user-read-playback-state", "user-modify-playback-state"))
+            .build()
+
+        AuthenticationClient.openLoginActivity(this, Constants.AUTH_TOKEN_REQUEST_CODE, request)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (Constants.AUTH_TOKEN_REQUEST_CODE == requestCode) {
+            val response = AuthenticationClient.getResponse(resultCode, data)
+            if (response.accessToken != null) {
+                SpotifyToken.setToken(response.accessToken)
+                SpotifyToken.fetchingToken = false
+            } else {
+                Log.e("SpotifyConnection", "something went wrong with authentication")
+            }
+            finish()
+        }
+    }
+}
--- app/src/main/java/no/birg/albumselector/SpotifyConnection.kt
+++ app/src/main/java/no/birg/albumselector/SpotifyConnection.kt
@@ -1,12 +1,10 @@
 package no.birg.albumselector
 
 import android.app.Activity
+import android.app.Service
 import android.content.Intent
-import android.os.Bundle
+import android.os.IBinder
 import android.util.Log
-import com.spotify.sdk.android.authentication.AuthenticationClient
-import com.spotify.sdk.android.authentication.AuthenticationRequest
-import com.spotify.sdk.android.authentication.AuthenticationResponse
 import kotlinx.coroutines.*
 import org.json.JSONArray
 import org.json.JSONObject
@@ -15,36 +13,16 @@ import java.net.URLEncoder
 import java.nio.charset.Charset
 import javax.net.ssl.HttpsURLConnection
 
-class SpotifyConnection : Activity() {
+class SpotifyConnection : Service() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val request = AuthenticationRequest.Builder(Constants.CLIENT_ID, AuthenticationResponse.Type.TOKEN, Constants.REDIRECT_URI)
-            .setShowDialog(false)
-            .setScopes(arrayOf("user-read-email", "user-read-private", "user-read-playback-state", "user-modify-playback-state"))
-            .build()
-
-        AuthenticationClient.openLoginActivity(this, Constants.AUTH_TOKEN_REQUEST_CODE, request)
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
     }
 
     fun fetchAccessToken(activity: Activity) {
         SpotifyToken.fetchingToken = true
-        val intent = Intent(activity, SpotifyConnection::class.java)
+        val intent = Intent(activity, SpotifyAuthenticationActivity::class.java)
         activity.startActivityForResult(intent, 1)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (Constants.AUTH_TOKEN_REQUEST_CODE == requestCode) {
-            val response = AuthenticationClient.getResponse(resultCode, data)
-            if (response.accessToken != null) {
-                SpotifyToken.setToken(response.accessToken)
-                SpotifyToken.fetchingToken = false
-            } else {
-                Log.e("SpotifyConnection", "something went wrong with authentication")
-            }
-            finish()
-        }
     }
 
     fun fetchUsername(activity: Activity, retry: Boolean = false) : String = runBlocking {
