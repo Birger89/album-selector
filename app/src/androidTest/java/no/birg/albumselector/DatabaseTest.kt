e0407d6dd3ab4aaffebee19afcb773815fb19ff6
Birger Topphol <btopphol@hotmail.com>
2020-09-16 16:55:42 +0200

    Update DB tests for LiveData

--- app/src/androidTest/java/no/birg/albumselector/DatabaseTest.kt
+++ app/src/androidTest/java/no/birg/albumselector/DatabaseTest.kt
@@ -1,19 +1,25 @@
 package no.birg.albumselector
 
 import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import no.birg.albumselector.database.*
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TestRule
 import org.junit.runner.RunWith
 import java.io.IOException
 import kotlin.test.assertEquals
 
 @RunWith(AndroidJUnit4::class)
 class DatabaseTest {
+    @get:Rule
+    val rule: TestRule = InstantTaskExecutorRule()
+
     private lateinit var db: AppDatabase
     private lateinit var albumDao: AlbumDao
     private lateinit var categoryDao: CategoryDao
@@ -55,10 +61,14 @@ class DatabaseTest {
     fun addAndDeleteAlbum() {
         val album = getTestAlbum()
         albumDao.insert(album)
-        assertEquals(album, albumDao.getAll()[0])
+
+        val album2 = albumDao.getAll().getOrAwaitValue()[0]
+        assertEquals(album, album2)
 
         albumDao.delete(album)
-        assertEquals(0, albumDao.getAll().size)
+
+        val albums = albumDao.getAll().getOrAwaitValue()
+        assertEquals(0, albums.size)
     }
 
     @Test
@@ -80,13 +90,16 @@ class DatabaseTest {
         categoryDao.insertAlbumCrossRef(CategoryAlbumCrossRef(TEST_CID, TEST_AID))
 
         val album = albumDao.getAllWithCategories()[0]
-        val category = categoryDao.getAllWithAlbums()[0]
+        val categories = categoryDao.getAllWithAlbums().getOrAwaitValue()
         assertEquals(TEST_CID, album.categories[0].cid)
-        assertEquals(TEST_AID, category.albums[0].aid)
+        assertEquals(TEST_AID, categories[0].albums[0].aid)
 
         categoryDao.deleteAlbumCrossRef(CategoryAlbumCrossRef(TEST_CID, TEST_AID))
-        assertEquals(0, categoryDao.getAllWithAlbums()[0].albums.size)
-        assertEquals(0, albumDao.getAllWithCategories()[0].categories.size)
+
+        val album2 = albumDao.getAllWithCategories()[0]
+        val categories2 = categoryDao.getAllWithAlbums().getOrAwaitValue()
+        assertEquals(0, categories2[0].albums.size)
+        assertEquals(0, album2.categories.size)
     }
 
     @Test
@@ -97,8 +110,10 @@ class DatabaseTest {
         categoryDao.insertAlbumCrossRef(CategoryAlbumCrossRef(TEST_CID, TEST_AID))
 
         albumDao.delete(getTestAlbum())
-        assertEquals(1, categoryDao.getAll().size)
-        assertEquals(0, categoryDao.getAllWithAlbums()[0].albums.size)
+
+        val categories = categoryDao.getAllWithAlbums().getOrAwaitValue()
+        assertEquals(1, categories.size)
+        assertEquals(0, categories[0].albums.size)
     }
 
     @Test
@@ -109,7 +124,9 @@ class DatabaseTest {
         categoryDao.insertAlbumCrossRef(CategoryAlbumCrossRef(TEST_CID, TEST_AID))
 
         categoryDao.delete(getTestCategory())
-        assertEquals(1, albumDao.getAll().size)
-        assertEquals(0, albumDao.getAllWithCategories()[0].categories.size)
+
+        val albums = albumDao.getAllWithCategories()
+        assertEquals(1, albums.size)
+        assertEquals(0, albums[0].categories.size)
     }
 }
